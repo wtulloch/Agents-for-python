1061d119b689a5b685d9924e9ae06a6b9f9d2e82
Axel Suarez Martinez <axsuarez@microsoft.com>
2024-12-05 11:56:02 -0600

    Initial commit adding basic lib structure.

---
+++ libraries/Core/Protocols/microsoft-agents-protocol/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='microsoft-agents-protocol',
+    version='0.0.0a1',
+    packages=find_namespace_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    author='Microsoft Corporation',
+    description='A protocol library for Microsoft Agents',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/microsoft/microsoft-agents-protocol',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
