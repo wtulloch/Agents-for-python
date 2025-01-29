64f68f4d38a1619205320fbd27156a0eb11167a6
Axel Suarez <axsuarez@microsoft.com>
2025-01-29 13:16:05 -0800

    WIP Connection infra

--- libraries/Botbuilder/microsoft-agents-botbuilder/microsoft/agents/botbuilder/rest_channel_service_client_factory.py
+++ libraries/Botbuilder/microsoft-agents-botbuilder/microsoft/agents/botbuilder/rest_channel_service_client_factory.py
@@ -1,12 +1,28 @@
-from typing import Optional
+from typing import Any, Optional
 
-from microsoft.agents.authentication import ClaimsIdentity
+from microsoft.agents.authentication import (
+    AuthenticationConstants,
+    ClaimsIdentity,
+    Connections,
+)
 from microsoft.agents.connector import ConnectorClientBase
 
 from .channel_service_client_factory_base import ChannelServiceClientFactoryBase
 
 
 class RestChannelServiceClientFactory(ChannelServiceClientFactoryBase):
+    def __init__(
+        self,
+        configuration: Any,
+        connections: Connections,
+        token_service_endpoint=AuthenticationConstants.BOT_FRAMEWORK_OAUTH_URL,
+        token_service_audience=AuthenticationConstants.BOT_FRAMEWORK_SCOPE,
+        **kwargs: Any
+    ) -> None:
+        self._connections = connections
+        self._token_service_endpoint = token_service_endpoint
+        self._token_service_audience = token_service_audience
+
     async def create_connector_client(
         self,
         claims_identity: ClaimsIdentity,
--- libraries/Core/microsoft-agents-authentication/microsoft/agents/authentication/__init__.py
+++ libraries/Core/microsoft-agents-authentication/microsoft/agents/authentication/__init__.py
@@ -1,10 +1,12 @@
 from .authentication_constants import AuthenticationConstants
+from .connections import Connections
 from .bot_auth_configuration import BotAuthConfiguration
 from .claims_identity import ClaimsIdentity
 from .jwt_token_validator import JwtTokenValidator
 
 __all__ = [
     "AuthenticationConstants",
+    "Connections",
     "BotAuthConfiguration",
     "ClaimsIdentity",
     "JwtTokenValidator",
